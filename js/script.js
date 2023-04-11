21e6cb59b09f70962c2963d739f3ab0cb5bee754
GersonMembrillo <gersonmembrillo@hotmail.com>
2023-04-11 21:56:46 +0200

    add vue parts & api (still miss bootstrap)

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const { createApp } = Vue;
+
+createApp({
+	data() {
+		return {
+			titolo: 'Vue Email List',
+			randomMail: [],
+			basePath: 'https://flynn.boolean.careers/exercises/api/',
+		};
+	},
+	methods: {
+		getData() {
+			this.randomMail = [];
+			for (let i = 0; i < 10; i++) {
+				axios.get(this.basePath + 'random/mail').then((res) => {
+					console.log(res.data.response);
+					this.randomMail.push(res.data.response);
+				});
+			}
+		},
+	},
+	mounted() {
+		this.getData();
+	},
+}).mount('#app');
